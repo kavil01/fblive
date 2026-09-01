256479e45aef0d03160d4e9dfc8ee5e00b0097ee
kavil01 <122100573+kavil01@users.noreply.github.com>
2026-09-01 14:16:51 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# yt-dlp கருவியை பதிவிறக்கம் செய்து புதுப்பித்தல்
-sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
-sudo chmod a+rx /usr/local/bin/yt-dlp
+# Virtual Screen அமைத்தல்
+Xvfb :99 -screen 0 1280x720x24 &
+export DISPLAY=:99
+sleep 2
 
-# உங்கள் Blogger தளத்தில் இயங்கும் YouTube Video / Live Link-ஐ கீழே உள்ள URL-ல் மாற்றவும்
-YOUTUBE_URL="https://www.youtube.com/watch?v=YOUR_VIDEO_ID"
+# Fluxbox Window Manager
+fluxbox &
+sleep 2
 
-# Direct Stream Link-ஐ எடுத்து FFmpeg மூலம் நேரலையாக ஸ்ட்ரீம் செய்தல்
-STREAM_URL=$(yt-dlp -g -f "best[ext=mp4]/best" "$YOUTUBE_URL")
+# Node.js மூலம் Puppeteer திரையை இயக்குதல்
+node stream.js &
+sleep 25
 
-ffmpeg -re -i "$STREAM_URL" \
-       -c:v libx264 -preset ultrafast -b:v 2500k -maxrate 2500k -bufsize 5000k \
-       -pix_fmt yuv420p -g 60 -c:a aac -b:a 128k -ar 44100 \
+# FFmpeg மூலம் உங்கள் வெப்சைட் திரையை Facebook-க்கு ஸ்ட்ரீம் செய்தல்
+ffmpeg -f x11grab -video_size 1280x720 -i :99.0 \
+       -f lavfi -i anullsrc \
+       -c:v libx264 -preset ultrafast -pix_fmt yuv420p -g 60 -b:v 2500k \
+       -c:a aac -b:a 128000 -ar 44100 \
        -f flv "rtmps://live-api-s.facebook.com:443/rtmp/${FB_STREAM_KEY}"
