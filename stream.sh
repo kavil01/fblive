12d16f948e2be4d5f82fe90b870f15b276dc70a8
kavil01 <122100573+kavil01@users.noreply.github.com>
2026-09-01 12:41:21 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# டிஸ்ப்ளே அமைவு
+# Virtual Screen 1280x720 24-bit அமைத்தல்
 Xvfb :99 -screen 0 1280x720x24 &
+sleep 2
 export DISPLAY=:99
 
-# Google Chrome பின்னணியில் திறத்தல்
-google-chrome --no-sandbox --disable-setuid-sandbox --headless=new --remote-debugging-port=9222 --window-size=1280,720 "https://kingfmtamilgroup.blogspot.com/" &
-sleep 10
+# Chromium உலாவியை முழுத் திரையில் (Kiosk Mode) திறத்தல்
+chromium-browser \
+  --no-sandbox \
+  --disable-setuid-sandbox \
+  --disable-dev-shm-usage \
+  --start-fullscreen \
+  --kiosk \
+  --window-position=0,0 \
+  --window-size=1280,720 \
+  --autoplay-policy=no-user-gesture-required \
+  "https://kingfmtamilgroup.blogspot.com/" &
 
-# FFmpeg மூலம் Facebook-க்கு லைவ் ஸ்ட்ரீம் செய்தல்
+# வெப்சைட் முழுமையாக Load ஆக 15 வினாடிகள் காத்திருத்தல்
+sleep 15
+
+# FFmpeg மூலம் திரையைப் பிடித்து Facebook-க்கு ஸ்ட்ரீம் செய்தல்
 ffmpeg -f x11grab -video_size 1280x720 -i :99.0 \
        -f lavfi -i anullsrc \
        -c:v libx264 -preset ultrafast -pix_fmt yuv420p -g 60 -b:v 2500k \
