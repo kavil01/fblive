f5b3335f48e5f03aa741ca52fd12a7d183519d30
kavil01 <122100573+kavil01@users.noreply.github.com>
2026-09-01 15:01:24 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-const fs = require('fs');
 
 puppeteer.use(StealthPlugin());
 
@@ -12,32 +11,21 @@ puppeteer.use(StealthPlugin());
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-blink-features=AutomationControlled',
-      '--start-fullscreen',
-      '--window-size=1280,720',
-      '--autoplay-policy=no-user-gesture-required'
+      // இலவச Proxy IP மூலம் Request அனுப்புதல்
+      '--proxy-server=http://104.207.135.226:3128',
+      '--window-size=1280,720'
     ],
     defaultViewport: { width: 1280, height: 720 }
   });
 
   const page = await browser.newPage();
 
-  // 1. YouTube Cookies ஏற்றுதல்
-  if (fs.existsSync('cookies.json')) {
-    const cookies = JSON.parse(fs.readFileSync('cookies.json', 'utf8'));
-    await page.setCookie(...cookies);
-  }
-
-  // 2. Browser User-Agent அமைத்தல்
   await page.setUserAgent(
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
   );
 
-  try {
-    await page.goto('https://kingfmtamilgroup.blogspot.com/', {
-      waitUntil: 'networkidle2',
-      timeout: 90000
-    });
-  } catch (err) {
-    console.error(err);
-  }
+  await page.goto('https://kingfmtamilgroup.blogspot.com/', {
+    waitUntil: 'networkidle2',
+    timeout: 90000
+  });
 })();
